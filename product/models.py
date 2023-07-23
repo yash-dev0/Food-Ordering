5c43c52d526f79878cb52f9995cd5863ac67a780
yash-dev0 <yashpal902720@gmail.com>
2023-07-23 18:35:02 +0530

    added product image model in models.py

--- product/models.py
+++ product/models.py
@@ -4,6 +4,13 @@ import uuid
 
 class Product(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    product_name = models.CharField(max_length=100) 
     create_at = models.DateField(auto_created=True)
     update_at = models.DateField(auto_created=True)
-    product_name = models.CharField(max_length=100)
+
+
+class ProductImages(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    product_images = models.ImageField(upload_to='templates/product_images')
+    create_at = models.DateField(auto_created=True)
+    update_at = models.DateField(auto_created=True)
