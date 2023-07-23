69f04dacd7b99451525f2de6fe0e6ed69fbf6bbd
yash-dev0 <yashpal902720@gmail.com>
2023-07-23 18:29:03 +0530

    created one product model in models.py

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Product(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    create_at = models.DateField(auto_created=True)
+    update_at = models.DateField(auto_created=True)
+    product_name = models.CharField(max_length=100)
